c9a267428561f34b34d6d93265d8b5f4900cdecc
vutuanhung <vutuanhung.fcbs@gmail.com>
2021-04-06 18:24:27 +0700

    danh sach mon an + branch (chỉnh sửa)

--- back_end/branch.php
+++ back_end/branch.php
@@ -1,22 +1,43 @@
 <?php
-    include 'connect.php';
+    function listBranches() {
+        include 'connect.php';
 
-    $sql = "SELECT ten_chi_nhanh FROM chi_nhanh";
-    $result = $conn->query($sql) ;
-    $list_chi_nhanh = array();
-    while($row = $result->fetch_assoc()) {
-        $list_chi_nhanh[] = $row["ten_chi_nhanh"];
+        
+        //lay ma khu vuc
+        $khu_vuc = array();
+        $sql = "SELECT DISTINCT idkhu_vuc,ten_khu_vuc FROM khu_vuc";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            $idkhu_vuc[$row["ten_khu_vuc"]] = $row["idkhu_vuc"];
+        } 
+
+        // echo "<pre>";
+        // print_r($idkhu_vuc);
+        // echo "<pre>";
+
+        //
+        $list_chi_nhanh = array();
+        foreach($idkhu_vuc as $ten => $id) {
+            $chi_nhanh = array();
+            $sql = "SELECT ten_chi_nhanh FROM chi_nhanh WHERE idkhu_vuc = $id";
+            $result = $conn->query($sql);
+            while($row = $result->fetch_assoc()) {
+                $chi_nhanh[] = $row["ten_chi_nhanh"];
+            } 
+            $list_chi_nhanh[$ten] = $chi_nhanh;
+        }
+
+        $conn->close();
+
+        return $list_chi_nhanh;
     }
 
-    echo "in chi nhánh <br>";
-    echo "<pre>";
-    print_r($list_chi_nhanh);
-    echo "</pre>";
-    $conn->close();
+    //$list_chi_nhanh = listBranches();
+    //// for($i=0 ; $i<count($list_chi_nhanh) ; $i++) {
+    ////     echo $list_chi_nhanh[$i] . "<br>";
+    //// }
 
-    // for($i=0 ; $i<count($list_chi_nhanh) ; $i++) {
-    //     echo $list_chi_nhanh[$i] . "<br>";
-    // }
-
-    
+    // echo "<pre>";
+    // print_r($list_chi_nhanh);
+    // echo "<pre>";
 ?>
---
+++ back_end/dish.php
@@ -0,0 +1,42 @@
+<?php
+    function listDish(){
+        include 'connect.php';
+
+        //lay ma thuc don
+        $list_thuc_don = array();
+        $sql = "SELECT DISTINCT idthuc_don,ten_thuc_don FROM thuc_don";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            $list_thuc_don[$row["ten_thuc_don"]] = $row["idthuc_don"];
+        } 
+
+        // echo "<pre>";
+        // print_r($list_thuc_don);
+        // echo "<pre>";
+
+        //
+        $list_mon_an = array();
+        foreach($list_thuc_don as $ten => $id) {
+            $mon_an = array();
+            $sql = "SELECT idmon_an, gia, mo_ta, ten_mon_an FROM mon_an WHERE idthuc_don = $id";
+            $result = $conn->query($sql);
+            while($row = $result->fetch_assoc()) {
+                $detail_mon_an = array();
+                $detail_mon_an['id'] = $row["idmon_an"];
+                $detail_mon_an['gia'] = $row["gia"];
+                $detail_mon_an['mo_ta'] = $row["mo_ta"];
+                $mon_an[$row["ten_mon_an"]]  = $detail_mon_an;
+            } 
+            $list_mon_an[$ten] = $mon_an;
+        }
+
+        $conn->close();
+
+        return $list_mon_an;
+    }
+
+    // $list_mon_an = listDish();
+    // echo "<pre>";
+    // print_r($list_mon_an);
+    // echo "<pre>";
+?>
