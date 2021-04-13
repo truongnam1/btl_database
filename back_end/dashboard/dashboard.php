a609c45eee698bd62675bf3eac190268dad31a07
truongnam1 <62241216+truongnam1@users.noreply.github.com>
2021-04-13 15:49:09 +0700

    thêm phần dashboard

---
+++ back_end/dashboard/dashboard.php
@@ -0,0 +1,57 @@
+<?php 
+
+
+/**
+ * lấy TỔNG SỐ KHÁCH DỰ KIẾN(của tháng này) 
+ * TỔNG SỐ KHÁCH DỰ KIẾN là cộng tổng số người trong form đặt bàn
+ * return : tổng số người (số)
+ */
+function f0() {
+
+
+}
+
+/**
+ * trả về tổng số đơn đặt bàn của tháng hiện tại
+ * mỗi form đc tính là 1 đơn
+ * return: tổng số đơn tháng này (số)
+ */
+function f1 () {
+
+
+}
+
+/**
+ * trả về tổng số đơn đặt bàn của tháng này ở trạng thái phê duyệt
+ * mỗi form đc tính là 1 đơn
+ * return: tổng số đơn đã phê duyệt tháng hiện tại (số)
+ */
+function f2() {
+
+}
+
+/**
+ * trả về tổng số đơn đặt bàn ở trạng thái chưa xử lý (tất cả các đơn từ trước đến hiện tại)
+ * mỗi form đc tính là 1 đơn
+ * return: tổng số đơn đã phê duyệt tháng hiện tại (số)
+ */
+function f3() {
+
+}
+
+
+/**
+ * trả về 1 array gồm 12 phần tử, key là tên tháng, value là tổng số lượng đơn của tháng đó
+ * 
+ */
+function f4() {
+
+}
+
+
+
+
+
+
+
+?>
