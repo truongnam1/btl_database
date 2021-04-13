7cfb48f7ae02d8f15c68e6dc02f677fea5495306
vutuanhung <vutuanhung.fcbs@gmail.com>
2021-04-13 22:07:44 +0700

    dashboard (lỗi include chưa fix đc)

---
+++ back_end/dashboard/connect.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "103.97.125.251"; 
+    $username = "truongna_hung_btl_db"; 
+    $password = "y8Xx5Q7cZpfv9pBx";   
+    $dbname = "truongna_nam_btl_db"; 
+    //create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    mysqli_query($conn,"set names utf8");
+    // check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        // echo "connect successfully";
+    }
+    echo "<br>";
+
+        
+?>
--- back_end/dashboard/dashboard.php
+++ back_end/dashboard/dashboard.php
@@ -1,5 +1,5 @@
 <?php 
-
+    //include_once '../back_end/connect.php';
 
 /**
  * lấy TỔNG SỐ KHÁCH DỰ KIẾN(của tháng này) 
@@ -7,9 +7,25 @@
  * return : tổng số người (số)
  */
 function f0() {
+    include 'connect.php';
 
+    $count = 0;
+    $present = getdate();
+    $year = $present['year'];
+    $month = $present['mon'];
+    // print_r(getdate());
+    // echo $present['mon'] + $present['year'];
+    $sql = "SELECT so_nguoi FROM dat_ban
+    WHERE EXTRACT(YEAR FROM ngay_dat) = $year AND EXTRACT(MONTH FROM ngay_dat) = $month";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        $count += $row["so_nguoi"];
+    }
 
+    return $count;
 }
+
+//echo f0();
 
 /**
  * trả về tổng số đơn đặt bàn của tháng hiện tại
@@ -17,17 +33,44 @@ function f0() {
  * return: tổng số đơn tháng này (số)
  */
 function f1 () {
+    include 'connect.php';
 
+    $count = 0;
+    $present = getdate();
+    $year = $present['year'];
+    $month = $present['mon'];
+    // print_r(getdate());
+    // echo $present['mon'] + $present['year'];
+    $sql = "SELECT COUNT(*) AS total FROM dat_ban
+    WHERE EXTRACT(YEAR FROM ngay_dat) = $year AND EXTRACT(MONTH FROM ngay_dat) = $month";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $count = $row['total'];
+
+    return $count;
 
 }
 
+echo f1();
 /**
  * trả về tổng số đơn đặt bàn của tháng này ở trạng thái phê duyệt
  * mỗi form đc tính là 1 đơn
  * return: tổng số đơn đã phê duyệt tháng hiện tại (số)
  */
 function f2() {
+    include 'connect.php';
 
+    $count = 0;
+    $present = getdate();
+    $year = $present['year'];
+    $month = $present['mon'];
+    $sql = "SELECT COUNT(*) AS total FROM dat_ban
+    WHERE EXTRACT(YEAR FROM ngay_dat) = $year AND EXTRACT(MONTH FROM ngay_dat) = $month AND trang_thai = 'phê duyệt'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $count = $row['total'];
+
+    return $count;
 }
 
 /**
@@ -36,7 +79,16 @@ function f2() {
  * return: tổng số đơn đã phê duyệt tháng hiện tại (số)
  */
 function f3() {
+    include 'connect.php';
 
+    $count = 0;
+    $sql = "SELECT COUNT(*) AS total FROM dat_ban
+    WHERE trang_thai = 'chưa xử lý'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $count = $row['total'];
+
+    return $count;
 }
 
 
@@ -45,8 +97,27 @@ function f3() {
  * 
  */
 function f4() {
-
+    include 'connect.php';
+    
+    $list = array();
+    for($i=1 ; $i<=12 ; $i++) {
+        $list[$i] = 0;
+    }
+    $sql = "SELECT MONTH(ngay_dat) AS month, COUNT(*) AS count 
+            FROM dat_ban
+            GROUP BY month
+            ORDER BY month";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        $list[$row["month"]] = $row["count"];
+    }
+    
+    return $list;
 }
+
+// echo "<pre>";
+// print_r(f4());
+// echo "<pre>";
 
 
 
