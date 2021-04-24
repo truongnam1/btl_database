3698bf81c09781fa384290c75b1a1cfc92b407da
vutuanhung <vutuanhung.fcbs@gmail.com>
2021-04-24 21:08:50 +0700

    Update detailCombo.php

--- back_end/detailCombo.php
+++ back_end/detailCombo.php
@@ -23,16 +23,47 @@
     // print_r(detailCombo(101));
     // echo "<pre>";
 
-    // echo "<pre>";
-    // print_r(detailCombo(102));
-    // echo "<pre>";
+    function detailNotInCombo(){
+        include 'connect.php';
+
+        $list = array();
+        $fullId = array();
+        $idInCombo = array();
+        $idNotInCombo;
+        //
+        $sql = "SELECT idmon_an FROM mon_an";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            $fullId[] = $row['idmon_an'];
+        }
+
+        // 
+        $sql = "SELECT DISTINCT idmon_an FROM combo_monan";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            $idInCombo[] = $row['idmon_an'];
+        }
+        $idNotInCombo = array_diff($fullId, $idInCombo);
+
+        //
+        foreach($idNotInCombo as $value) {
+            $sql = "SELECT mon_an.gia, mon_an.mo_ta, mon_an.ten_mon_an, mon_an.link_image 
+                    FROM mon_an 
+                    WHERE idmon_an = $value";
+            $result = $conn->query($sql);
+            while($row = $result->fetch_assoc()) {
+                $dish = array();
+                $dish['gia'] = $row["gia"];
+                $dish['mo_ta'] = $row["mo_ta"];
+                $dish['link_image'] = $row["link_image"];
+                $list[$row["ten_mon_an"]] = $dish;
+            }
+        }
+
+        return $list;
+    }
 
     // echo "<pre>";
-    // print_r(detailCombo(103));
+    // print_r(detailNotInCombo());
     // echo "<pre>";
-
-    // echo "<pre>";
-    // print_r(detailCombo(201));
-    // echo "<pre>";
-
 ?>
